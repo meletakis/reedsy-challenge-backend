79c9c17d53e87a650798810a9d595f348f733e79
Giannis Meletakis <meletakis@gmail.com>
2023-10-20 22:05:35 +0300

    Migration: Introduce products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_20_184618) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_20_190043) do
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.integer "category_id"
+    t.string "name", null: false
+    t.integer "price_in_cents", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  add_foreign_key "products", "categories"
 end
